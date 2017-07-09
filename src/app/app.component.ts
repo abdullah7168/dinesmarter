fbfa51b5d7faced397a21b12964172cb85062010
Malik Waleed <waleed_tfps@hotmail.com>
2017-07-09 08:02:10 +0500

    map changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,49 +4,101 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-
+import { Geolocation } from '@ionic-native/geolocation';
+import { LoadingController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { Storage } from '@ionic/storage';
+import 'rxjs/add/operator/map';
 
 @Component({
   templateUrl: 'app.html'
 })
+
+
+
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
   pages: Array<{title: string, component: any}>;
+  public test=[];
+
+  public data: any;
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
   //data arrays def
-  allergens: Array<{id: string, allergen: any}>;
+  allergens: any;
   cusines: Array<{id: string, cusine: any}>;
   services: Array<{id: string, serviceStyle: any}>;
   //selected val
   selectedalgs: Array<{id: string, allergen: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(private storage: Storage,public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,public geo: Geolocation,public loadingCtrl: LoadingController,public http: Http) {
    
     this.initializeApp();
-
+this.test.push("waleed");
+var duplicates = [1,3,4,2,1,2,3,8];
+var duplicates = duplicates.filter(function(item, pos) {
+    return duplicates.indexOf(item) == pos;
+})
+console.log(duplicates);
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
     ];
     // init data arrays
-    this.allergens = [
-      {id: '1',allergen: 'Peanuts'},
-      {id: '2',allergen: 'Mushrooms'},
-      {id: '3',allergen: 'Woffels'},
-      {id: '4',allergen: 'Nuts'},
-    ];
-    this.cusines = [
-      {id: '1',cusine: 'Chinese'},
-    ];
-    this.services = [
-      {id: '1',serviceStyle: 'Desi'},
-    ];
+    let loading = this.loadingCtrl.create({
+    content: 'Trying to get your location...'
+  });
 
-    //selected val
-    this.selectedalgs = [
-      {id:'1',allergen: 'Peanuts'},
-    ];
+  loading.present();
+
+  setTimeout(() => {
+    
+     this.geo.getCurrentPosition().then((resp) => {
+ // resp.coords.latitude
+ // resp.coords.longitude
+ this.lat=resp.coords.latitude;
+ this.lng=resp.coords.longitude;
+
+ //alert(resp.coords.latitude+" "+resp.coords.longitude);
+ //alert(resp.coords.latitude);
+
+
+ this.http.get('http://menudriven.net/prototype/api/appload')
+      .map(res => res.json())
+      .subscribe(data => {
+        // we've got back the raw data, now generate the core schedule data
+        // and save the data for later reference
+        
+        //this.storage.set("lat",data.allergens);
+        // this.storage.set("cuisines",data.cuisines);
+        // this.storage.set("service",data.service);
+        this.allergens=data.allergens;
+        this.cusines=data.cuisines;
+        this.services=data.service;
+        console.log(data.allergens);
+        loading.dismiss();
+        this.nav.setRoot(HomePage,{lat:this.lat,lng:this.lng});
+        console.log({lat:this.lat,lng:this.lng});
+    }, err => {
+         loading.dismiss();
+        console.log("Oops!");
+        this.platform.exitApp();
+    });
+
+
+
+
+}).catch((error) => {
+  loading.dismiss();
+  this.platform.exitApp();
+ // alert('Error getting location');
+});
+  }, 3000);
+
+
 
   }
   compareAllergens(allergens, selectedalgs){
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,63 +1,71 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { Storage } from '@ionic/storage';
 import 'rxjs/add/operator/map';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
 public data: any;
-lat: number = 51.678418;
-  lng: number = 7.809007;
+public lat: number = 51.678418;
+public lng: number = 7.809007;
 
-  constructor(private storage: Storage,public navCtrl: NavController,public geo: Geolocation,public loadingCtrl: LoadingController,public http: Http) {
+  constructor(private storage: Storage,public navCtrl: NavController,public geo: Geolocation,public loadingCtrl: LoadingController,public http: Http,public navParams: NavParams) {
  
- let loading = this.loadingCtrl.create({
-    content: 'Trying to get your location...'
-  });
+this.lat=this.navParams.get('lat');
+this.lng=this.navParams.get('lng');
+console.log(this.lat);
 
-  loading.present();
 
-  setTimeout(() => {
+
+//  let loading = this.loadingCtrl.create({
+//     content: 'Trying to get your location...'
+//   });
+
+//   loading.present();
+
+//   setTimeout(() => {
     
-     this.geo.getCurrentPosition().then((resp) => {
- // resp.coords.latitude
- // resp.coords.longitude
- this.lat=resp.coords.latitude;
- this.lng=resp.coords.longitude;
- //alert(resp.coords.latitude+" "+resp.coords.longitude);
- //alert(resp.coords.latitude);
+//      this.geo.getCurrentPosition().then((resp) => {
+//  // resp.coords.latitude
+//  // resp.coords.longitude
+//  this.lat=resp.coords.latitude;
+//  this.lng=resp.coords.longitude;
+//  //alert(resp.coords.latitude+" "+resp.coords.longitude);
+//  //alert(resp.coords.latitude);
 
 
- this.http.get('http://menudriven.net/prototype/api/appload')
-      .map(res => res.json())
-      .subscribe(data => {
-        // we've got back the raw data, now generate the core schedule data
-        // and save the data for later reference
+//  this.http.get('http://menudriven.net/prototype/api/appload')
+//       .map(res => res.json())
+//       .subscribe(data => {
+//         // we've got back the raw data, now generate the core schedule data
+//         // and save the data for later reference
         
-        this.storage.set("allergens",data.allergens);
-        this.storage.set("cuisines",data.cuisines);
-        this.storage.set("service",data.service);
-        console.log(data.allergens);
-        loading.dismiss();
-      }, err => {
-         loading.dismiss();
-        console.log("Oops!");
-    });
+//         this.storage.set("allergens",data.allergens);
+//         this.storage.set("cuisines",data.cuisines);
+//         this.storage.set("service",data.service);
+//         console.log(data.allergens);
+//         loading.dismiss();
+//       }, err => {
+//          loading.dismiss();
+//         console.log("Oops!");
+//     });
 
 
 
 
-}).catch((error) => {
-  loading.dismiss();
-  alert('Error getting location');
-});
-  }, 3000);
+// }).catch((error) => {
+//   loading.dismiss();
+//   alert('Error getting location');
+// });
+//   }, 3000);
 
 
 
